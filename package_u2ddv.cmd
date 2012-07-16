6ffca87a7fa14a18016cb046f42f7677315fc374
gian.schellenbaum@gmail.com <gian.schellenbaum@gmail.com@605c6810-b171-2bb6-8537-90259a8e6946>
2012-07-16 21:31:18 +0000

    git-svn-id: https://uberdemotools.googlecode.com/svn/trunk@10 605c6810-b171-2bb6-8537-90259a8e6946

---
+++ package_u2ddv.cmd
@@ -0,0 +1,7 @@
+PATH=%PATH%;C:\Program Files\WinRAR
+WinRAR.exe a -ep -rr10p -apbin u2ddv_bin.rar UDT_DV\bin\vs2010\release\U2DDV.exe
+WinRAR.exe a -ep -rr10p -apbin u2ddv_bin.rar UDT_DV\qt\dll_vs2010\*.dll
+WinRAR.exe a -ep -rr10p -apbin u2ddv_bin.rar UDT_DV\vs_crt_100\*.dll
+WinRAR.exe a -ep -rr10p -apdata\icons u2ddv_bin.rar UDT_DV\data\icons\*
+WinRAR.exe a -ep -rr10p -apdata\maps u2ddv_bin.rar UDT_DV\data\maps\*
+pause
