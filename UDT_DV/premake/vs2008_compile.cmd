f06b74f986d6cdca4f1bd6852a04f9fb1f89171a
gian.schellenbaum@gmail.com <gian.schellenbaum@gmail.com@605c6810-b171-2bb6-8537-90259a8e6946>
2012-07-16 19:07:59 +0000

    Fixed command-line compilation (wrong solution file name)
    git-svn-id: https://uberdemotools.googlecode.com/svn/trunk@5 605c6810-b171-2bb6-8537-90259a8e6946

--- UDT_DV/premake/vs2008_compile.cmd
+++ UDT_DV/premake/vs2008_compile.cmd
@@ -1,4 +1,4 @@
 call "%VS90COMNTOOLS%vsvars32.bat"
-"%VS90COMNTOOLS%..\IDE\devenv.exe" ..\build\vs2008\UDT.sln /Rebuild "Debug|Win32"
-"%VS90COMNTOOLS%..\IDE\devenv.exe" ..\build\vs2008\UDT.sln /Rebuild "Release|Win32"
+"%VS90COMNTOOLS%..\IDE\devenv.exe" ..\build\vs2008\U2DDV.sln /Rebuild "Debug|Win32"
+"%VS90COMNTOOLS%..\IDE\devenv.exe" ..\build\vs2008\U2DDV.sln /Rebuild "Release|Win32"
 pause
--- UDT_DV/premake/vs2010_compile.cmd
+++ UDT_DV/premake/vs2010_compile.cmd
@@ -1,4 +1,4 @@
 call "%VS100COMNTOOLS%vsvars32.bat"
-"%VS100COMNTOOLS%..\IDE\devenv.exe" ..\build\vs2010\UDT.sln /Rebuild "Debug|Win32"
-"%VS100COMNTOOLS%..\IDE\devenv.exe" ..\build\vs2010\UDT.sln /Rebuild "Release|Win32"
+"%VS100COMNTOOLS%..\IDE\devenv.exe" ..\build\vs2010\U2DDV.sln /Rebuild "Debug|Win32"
+"%VS100COMNTOOLS%..\IDE\devenv.exe" ..\build\vs2010\U2DDV.sln /Rebuild "Release|Win32"
 pause
