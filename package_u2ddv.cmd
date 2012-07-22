e4899a42da072161646417e8f9da0aec96fa8e31
gian.schellenbaum@gmail.com <gian.schellenbaum@gmail.com@605c6810-b171-2bb6-8537-90259a8e6946>
2012-07-22 22:24:30 +0000

    Added the inclusion of the data\weapons\* files
    git-svn-id: https://uberdemotools.googlecode.com/svn/trunk@34 605c6810-b171-2bb6-8537-90259a8e6946

--- package_u2ddv.cmd
+++ package_u2ddv.cmd
@@ -4,4 +4,5 @@ WinRAR.exe a -ep -rr10p -apbin u2ddv_bin.rar UDT_DV\qt\dll_vs2010\*.dll
 WinRAR.exe a -ep -rr10p -apbin u2ddv_bin.rar UDT_DV\vs_crt_100\*.dll
 WinRAR.exe a -ep -rr10p -apdata\icons u2ddv_bin.rar UDT_DV\data\icons\*
 WinRAR.exe a -ep -rr10p -apdata\maps u2ddv_bin.rar UDT_DV\data\maps\*
+WinRAR.exe a -ep -rr10p -apdata\weapons u2ddv_bin.rar UDT_DV\data\weapons\*
 pause
