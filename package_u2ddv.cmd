a2112eb4722d02ccacc96b72be1f81a7e4306189
gian.schellenbaum@gmail.com <gian.schellenbaum@gmail.com@605c6810-b171-2bb6-8537-90259a8e6946>
2012-07-29 15:43:46 +0000

    Added data/items/*
    git-svn-id: https://uberdemotools.googlecode.com/svn/trunk@118 605c6810-b171-2bb6-8537-90259a8e6946

--- package_u2ddv.cmd
+++ package_u2ddv.cmd
@@ -3,6 +3,7 @@ WinRAR.exe a -ep -rr10p -apbin u2ddv_bin.rar UDT_DV\bin\vs2010\release\U2DDV.exe
 WinRAR.exe a -ep -rr10p -apbin u2ddv_bin.rar UDT_DV\qt\dll_vs2010\*.dll
 WinRAR.exe a -ep -rr10p -apbin u2ddv_bin.rar UDT_DV\vs_crt_100\*.dll
 WinRAR.exe a -ep -rr10p -apdata\icons u2ddv_bin.rar UDT_DV\data\icons\*
+WinRAR.exe a -ep -rr10p -apdata\items u2ddv_bin.rar UDT_DV\data\items\*
 WinRAR.exe a -ep -rr10p -apdata\maps u2ddv_bin.rar UDT_DV\data\maps\*
 WinRAR.exe a -ep -rr10p -apdata\weapons u2ddv_bin.rar UDT_DV\data\weapons\*
 WinRAR.exe a -ep -rr10p u2ddv_bin.rar UDT_DV\legal\*
