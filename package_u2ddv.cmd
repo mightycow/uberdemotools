f2e6c7afb152e86d6b8498504dacfeda6465ebbd
gian.schellenbaum@gmail.com <gian.schellenbaum@gmail.com@605c6810-b171-2bb6-8537-90259a8e6946>
2012-07-30 23:37:30 +0000

    Linux files
    git-svn-id: https://uberdemotools.googlecode.com/svn/trunk@137 605c6810-b171-2bb6-8537-90259a8e6946

--- package_u2ddv.cmd
+++ package_u2ddv.cmd
@@ -1,4 +1,7 @@
 PATH=%PATH%;C:\Program Files\WinRAR
+WinRAR.exe a -ep -rr10p -apbin u2ddv_bin.rar U2DDV
+WinRAR.exe a -ep -rr10p -apbin u2ddv_bin.rar UDT_DV\qt\lib_gmake\*
+WinRAR.exe a -ep -rr10p -apbin u2ddv_bin.rar UDT_DV\linux_bin_script\*
 WinRAR.exe a -ep -rr10p -apbin u2ddv_bin.rar UDT_DV\bin\vs2010\release\U2DDV.exe
 WinRAR.exe a -ep -rr10p -apbin u2ddv_bin.rar UDT_DV\qt\dll_vs2010\*.dll
 WinRAR.exe a -ep -rr10p -apbin u2ddv_bin.rar UDT_DV\vs_crt_100\*.dll
