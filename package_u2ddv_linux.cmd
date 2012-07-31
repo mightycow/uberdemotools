ebe0f7069b2389647099077f20d2570baa7e1f5e
gian.schellenbaum@gmail.com <gian.schellenbaum@gmail.com@605c6810-b171-2bb6-8537-90259a8e6946>
2012-07-31 02:17:45 +0000

    git-svn-id: https://uberdemotools.googlecode.com/svn/trunk@142 605c6810-b171-2bb6-8537-90259a8e6946

---
+++ package_u2ddv_linux.cmd
@@ -0,0 +1,12 @@
+PATH=%PATH%;C:\Program Files\WinRAR
+WinRAR.exe a -ep -apbin U2DDV_lin.zip U2DDV
+WinRAR.exe a -ep -apbin U2DDV_lin.zip UDT_DV\qt\lib_gmake\*
+WinRAR.exe a -ep -apbin U2DDV_lin.zip UDT_DV\linux_bin_script\*
+WinRAR.exe a -ep -apdata\icons U2DDV_lin.zip UDT_DV\data\icons\*
+WinRAR.exe a -ep -apdata\items U2DDV_lin.zip UDT_DV\data\items\*
+WinRAR.exe a -ep -apdata\maps\q3 U2DDV_lin.zip UDT_DV\data\maps\q3\*
+WinRAR.exe a -ep -apdata\maps\ql U2DDV_lin.zip UDT_DV\data\maps\ql\*
+WinRAR.exe a -ep -apdata\weapons U2DDV_lin.zip UDT_DV\data\weapons\*
+WinRAR.exe a -ep U2DDV_lin.zip UDT_DV\legal\*
+WinRAR.exe a -ep U2DDV_lin.zip changelog_dv.txt
+pause
---
+++ package_u2ddv_windows.cmd
@@ -0,0 +1,12 @@
+PATH=%PATH%;C:\Program Files\WinRAR
+WinRAR.exe a -ep -rr10p -apbin U2DDV_win.rar UDT_DV\bin\vs2010\release\U2DDV.exe
+WinRAR.exe a -ep -rr10p -apbin U2DDV_win.rar UDT_DV\qt\dll_vs2010\*.dll
+WinRAR.exe a -ep -rr10p -apbin U2DDV_win.rar UDT_DV\vs_crt_100\*.dll
+WinRAR.exe a -ep -rr10p -apdata\icons U2DDV_win.rar UDT_DV\data\icons\*
+WinRAR.exe a -ep -rr10p -apdata\items U2DDV_win.rar UDT_DV\data\items\*
+WinRAR.exe a -ep -rr10p -apdata\maps\q3 U2DDV_win.rar UDT_DV\data\maps\q3\*
+WinRAR.exe a -ep -rr10p -apdata\maps\ql U2DDV_win.rar UDT_DV\data\maps\ql\*
+WinRAR.exe a -ep -rr10p -apdata\weapons U2DDV_win.rar UDT_DV\data\weapons\*
+WinRAR.exe a -ep -rr10p U2DDV_win.rar UDT_DV\legal\*
+WinRAR.exe a -ep -rr10p U2DDV_win.rar changelog_dv.txt
+pause
