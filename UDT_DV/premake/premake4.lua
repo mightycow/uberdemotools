3812b2f1f58f471f6a92abd9b8fc02b3f5f37343
gian.schellenbaum@gmail.com <gian.schellenbaum@gmail.com@605c6810-b171-2bb6-8537-90259a8e6946>
2012-08-01 00:37:37 +0000

    Option for using system Qt libs/includes Fixed file paths for chmod'ing the U2DDV executable
    git-svn-id: https://uberdemotools.googlecode.com/svn/trunk@149 605c6810-b171-2bb6-8537-90259a8e6946

--- UDT_DV/premake/premake4.lua
+++ UDT_DV/premake/premake4.lua
@@ -3,6 +3,48 @@
 -----------
 os.mkdir("../bin")
 
+-- Option QtUseSystemLibs
+newoption 
+{
+	trigger     = "QtUseSystemLibs",
+	value       = "false",
+	description = "Use the local system's Qt libs/includes?",
+	allowed = 
+	{
+		{ "false", "Use the provided Qt libs/includes (default behavior)" },
+		{ "true",  "Use the local system's Qt libs/includes" },
+	}
+}
+if not _OPTIONS.QtUseSystemLibs then
+	_OPTIONS.QtUseSystemLibs = "false"
+end
+print("QtUseSystemLibs? " .. _OPTIONS.QtUseSystemLibs)
+
+-- Define all the data required by option QtUseSystemLibs
+local uber_qt_paths_all = {}
+uber_qt_paths_all["false"]  =
+{
+	path_from_makefile = "../../qt/bin",
+	include_path = "../qt/include",
+	lib_path = "../qt/lib_" .. _ACTION,
+	core_lib_name = "QtCore4",
+	gui_lib_name = "QtGui4",
+}
+uber_qt_paths_all["true"]  =
+{
+	path_from_makefile = "/usr/bin",
+	include_path = "/usr/include/qt4",
+	lib_path = "/usr/lib",
+	core_lib_name = "libQtCore.so.4",
+	gui_lib_name = "libQtGui.so.4",
+}
+
+-- Select the data we must use
+local uber_qt_paths = uber_qt_paths_all[_OPTIONS.QtUseSystemLibs]
+if uber_qt_paths == nil then
+	error("Don't know where to look for Qt files!")
+end
+
 uber_ensure_file_exists = function(file_path)
 	if not os.isfile(file_path) then
 		os.copyfile("template_empty", file_path)
@@ -27,9 +69,8 @@ solution "U2DDV"
 
 	project "U2DDV"
 
-		local UBER_QT_BIN_PATH_FROM_MAKEFILE = "../../qt/bin"
 		uber_add_pre_build_command = function(tool_name, input_file, output_file)
-			prebuildcommands { path.translate(UBER_QT_BIN_PATH_FROM_MAKEFILE .. "/" .. tool_name .. " -o ../../src/qt_gen/" .. output_file .. " ../../src/qt/" .. input_file, nil) }
+			prebuildcommands { path.translate(uber_qt_paths.path_from_makefile .. "/" .. tool_name .. " -o ../../src/qt_gen/" .. output_file .. " ../../src/qt/" .. input_file, nil) }
 		end
 		
 		uber_add_pre_build_command("uic", "gui.ui", "gui_gen.hpp")
@@ -38,7 +79,7 @@ solution "U2DDV"
 		uber_add_pre_build_command("moc", "paint_widget.h", "paint_widget_gen.cpp")
 		
 		-- Create rsrc_gen.cpp from the resources listed in U2DDV.qrc and create a resource load function invoked like this: Q_INIT_RESOURCE(U2DDV);
-		prebuildcommands { path.translate(UBER_QT_BIN_PATH_FROM_MAKEFILE .. "/rcc -name U2DDV -o ../../src/qt_gen/rsrc_gen.cpp ../../rsrc/U2DDV.qrc", nil) }
+		prebuildcommands { path.translate(uber_qt_paths.path_from_makefile .. "/rcc -name U2DDV -o ../../src/qt_gen/rsrc_gen.cpp ../../rsrc/U2DDV.qrc", nil) }
 
 		location ("../build/" .. _ACTION)
 		kind "WindowedApp"
@@ -49,11 +90,11 @@ solution "U2DDV"
 		excludes { "../../UDT_DLL/src/api.cpp" } -- Make sure we don't even try to compile api.cpp (for DLL function export)
 		includedirs { "../src", "../src/qt", "../src/qt_gen" } -- Local headers
 		includedirs { "../../UDT_DLL/src" } -- DLL project headers
-		includedirs { "../qt/include" }
-		includedirs { "../qt/include/Qt" }
-		includedirs { "../qt/include/QtCore" }
-		includedirs { "../qt/include/QtGui" }
-		libdirs { "../qt/lib_" .. _ACTION }
+		includedirs { uber_qt_paths.include_path }
+		includedirs { uber_qt_paths.include_path .. "/Qt" }
+		includedirs { uber_qt_paths.include_path .. "/QtCore" }
+		includedirs { uber_qt_paths.include_path .. "/QtGui" }
+		libdirs { uber_qt_paths.lib_path }
 
 		flags { "Symbols", "NoNativeWChar", "NoPCH" } -- "NoRTTI", "StaticRuntime", "NoManifest", "ExtraWarnings", "FatalWarnings"
 		
@@ -93,18 +134,18 @@ solution "U2DDV"
 		-- GCC
 		--
 		configuration { "gmake" }
-			links { "QtCore4" }
-			links { "QtGui4" }
+			links { uber_qt_paths.core_lib_name }
+			links { uber_qt_paths.gui_lib_name }
 		
 		configuration { "Debug", "gmake" }
 			targetdir ( "../bin/gcc/debug" )
 			buildoptions { "" } -- Directly passed to the compiler.
 			linkoptions { "" } -- Directly passed to the linker.
-			postbuildcommands { "chmod +x ../bin/gcc/debug/U2DDV" }
+			postbuildcommands { "chmod +x ../../bin/gcc/debug/U2DDV" }
 			
 		configuration { "Release", "gmake" }
 			targetdir ( "../bin/gcc/release" )
 			buildoptions { "" } -- Directly passed to the compiler.
 			linkoptions { "" } -- Directly passed to the linker.
-			postbuildcommands { "chmod +x ../bin/gcc/release/U2DDV" }
+			postbuildcommands { "chmod +x ../../bin/gcc/release/U2DDV" }
 
