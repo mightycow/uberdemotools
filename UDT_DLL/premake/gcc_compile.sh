714093c910e167bc78ec813548f9e032b676073d
myt <myt@debian>
2014-12-02 13:36:43 -0500

    chg: the Linux build script now supports target and arch selection

--- UDT_DLL/premake/gcc_compile.sh
+++ UDT_DLL/premake/gcc_compile.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
 cd ../.build/gmake
 make clean
-make UDT config=release_x64
-make UDT_cutter config=release_x64
-make UDT_splitter config=release_x64
-make UDT_test_addons config=release_x64
+
+UDT_ARCH=""
+options=("x86", "x64")
+PS3="Select the architecture"
+select opt in "${options[@]}" "Quit"; do 
+    case "$REPLY" in
+    1 ) UDT_ARCH=x32;;
+    2 ) UDT_ARCH=x64;;
+    $(( ${#options[@]}+1 )) ) echo "Goodbye!"; break;;
+    *) echo "Invalid option. Try another one."; continue;;
+    esac
+	break
+done
+
+UDT_TARGET=""
+options=("Release", "Debug")
+PS3="Select the target"
+select opt in "${options[@]}" "Quit"; do 
+    case "$REPLY" in
+    1 ) UDT_TARGET=release;;
+    2 ) UDT_TARGET=debug;;
+    $(( ${#options[@]}+1 )) ) echo "Goodbye!"; break;;
+    *) echo "Invalid option. Try another one."; continue;;
+    esac
+	break
+done
+
+UDT_CONFIG="$UDT_TARGET"_"$UDT_ARCH"
+echo Selected config: $UDT_CONFIG
+
+make UDT config=$UDT_CONFIG
+make UDT_cutter config=$UDT_CONFIG
+make UDT_splitter config=$UDT_CONFIG
+make UDT_test_addons config=$UDT_CONFIG
+
