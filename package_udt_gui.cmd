095d000b2bc4d3a39d0dfda53fa76b529947a979
myT <mighty.cow@mailinator.com>
2014-12-05 05:58:21 +0100

    chg: updated packaging scripts chg: updated documentation

--- package_udt_gui.cmd
+++ package_udt_gui.cmd
@@ -1,4 +1,5 @@
 PATH=%PATH%;C:\Program Files\WinRAR
-WinRAR.exe a -ep -rr10p -zpackage_udt_gui_bin_comment.txt udt_bin.rar @package_udt_gui_bin.lst
+WinRAR.exe a -ep -rr10p -zpackage_udt_gui_bin_comment.txt udt_bin_x86.rar @package_udt_gui_bin_x86.lst
+WinRAR.exe a -ep -rr10p -zpackage_udt_gui_bin_comment.txt udt_bin_x64.rar @package_udt_gui_bin_x64.lst
 WinRAR.exe a -rr10p -zpackage_udt_gui_src_comment.txt udt_src.rar @package_udt_gui_src.lst
 pause
