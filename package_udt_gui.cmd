683eea6d242cc1027849245b2b2fbd709a755490
myT <mighty.cow@mailinator.com>
2014-12-07 09:31:54 +0100

    chg: packaging Windows binaries into .zip files add: script for packaging the Windows command-line tools

--- package_udt_gui.cmd
+++ package_udt_gui.cmd
@@ -1,5 +1,4 @@
 PATH=%PATH%;C:\Program Files\WinRAR
-WinRAR.exe a -ep -rr10p -zpackage_udt_gui_bin_comment.txt udt_bin_x86.rar @package_udt_gui_bin_x86.lst
-WinRAR.exe a -ep -rr10p -zpackage_udt_gui_bin_comment.txt udt_bin_x64.rar @package_udt_gui_bin_x64.lst
-WinRAR.exe a -rr10p -zpackage_udt_gui_src_comment.txt udt_src.rar @package_udt_gui_src.lst
+WinRAR.exe a -ep udt_gui_x86.zip @package_udt_gui_bin_x86.lst
+WinRAR.exe a -ep udt_gui_x64.zip @package_udt_gui_bin_x64.lst
 pause
