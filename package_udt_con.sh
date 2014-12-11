7a60eaae63f2992b58f4bae538c032f553b7ab5f
myt <myt@mailinator.com>
2014-12-10 23:04:12 -0500

    chg: now adding the changelog to the pre-compiled Linux packages

--- package_udt_con.sh
+++ package_udt_con.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-tar cvf udt_con_x86.tar.bz2 --bzip2 -p --xform s:UDT_DLL/.bin/gmake/x86/release:: UDT_DLL/.bin/gmake/x86/release/UDT_cutter UDT_DLL/.bin/gmake/x86/release/UDT_splitter
-tar cvf udt_con_x64.tar.bz2 --bzip2 -p --xform s:UDT_DLL/.bin/gmake/x64/release:: UDT_DLL/.bin/gmake/x64/release/UDT_cutter UDT_DLL/.bin/gmake/x64/release/UDT_splitter
+tar cvf udt_con_x86.tar.bz2 --bzip2 -p --xform s:UDT_DLL/.bin/gmake/x86/release:: UDT_DLL/.bin/gmake/x86/release/UDT_cutter UDT_DLL/.bin/gmake/x86/release/UDT_splitter --xform s:_dll:: changelog_dll.txt
+tar cvf udt_con_x64.tar.bz2 --bzip2 -p --xform s:UDT_DLL/.bin/gmake/x64/release:: UDT_DLL/.bin/gmake/x64/release/UDT_cutter UDT_DLL/.bin/gmake/x64/release/UDT_splitter --xform s:_dll:: changelog_dll.txt
 
