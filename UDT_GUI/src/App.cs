21a5ed97ddb3c8b112fc3d4e1da899d3891ce6c6
myT <mighty.cow@mailinator.com>
2014-12-11 02:37:55 +0100

    chg: bumped the GUI version up

--- UDT_GUI/src/App.cs
+++ UDT_GUI/src/App.cs
@@ -62,7 +62,7 @@ namespace Uber.DemoTools
 
     public class App
     {
-        private const string GuiVersion = "0.3.2";
+        private const string GuiVersion = "0.3.3";
         private readonly string DllVersion = UDT_DLL.GetVersion();
 
         private static readonly List<string> DemoExtensions = new List<string>
