b263a82bbbfd43dbc0bd0c1d9325efbb02f15944
myT <mighty.cow@mailinator.com>
2015-08-23 07:05:14 +0200

    fix: an inner loop counter variable was shadowing the outer loop counter variable

--- UDT_DLL/src/converter_udt_to_quake.cpp
+++ UDT_DLL/src/converter_udt_to_quake.cpp
@@ -197,9 +197,9 @@ bool udtdConverter::ProcessGameState()
 
 		_inStringData[length] = '\0';
 
-		for(u32 i = 0, count = _plugIns.GetSize(); i < count; ++i)
+		for(u32 j = 0, count = _plugIns.GetSize(); j < count; ++j)
 		{
-			_plugIns[i]->AnalyzeConfigString(index, _inStringData, (u32)length);
+			_plugIns[j]->AnalyzeConfigString(index, _inStringData, (u32)length);
 		}
 
 		_outMsg.WriteByte(svc_configstring);
