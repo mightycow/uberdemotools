0dfe1cf60604755cdb78b7475dc80afddb00f219
myT <mighty.cow@mailinator.com>
2015-10-15 16:49:27 +0200

    chg: UDT_merger: improved help text, return values and error output

--- UDT_DLL/src/apps/app_demo_merger.cpp
+++ UDT_DLL/src/apps/app_demo_merger.cpp
@@ -7,10 +7,18 @@
 #include <stdlib.h>
 
 
-static void PrintHelp()
+void PrintHelp()
 {
+	printf("For a set of two or more demos, creates a new demo using the entitystate\n");
+	printf("and playerstate snapshot data of all the specified demos.\n");
 	printf("\n");
-	printf("UDT_merger demo_path_1 demo_path_2 [demo_path_3 ... demo_path_n]\n");
+	printf("UDT_merger demopath1 demopath2 [demopath3 ... demopathn]\n");
+	printf("\n");
+	printf("Demo merging only works for matching server times.\n");
+	printf("In other words, you should only try to merge demos from the same match.\n");
+	printf("\n");
+	printf("The first-person view of the output demo will be that of the first demo\n");
+	printf("specified in the argument list.\n");
 }
 
 static int MergeDemos(char** filePaths, int fileCount)
@@ -23,7 +31,14 @@ static int MergeDemos(char** filePaths, int fileCount)
 	info.MessageCb = &CallbackConsoleMessage;
 	info.ProgressCb = &CallbackConsoleProgress;
 
-	return (udtMergeDemoFiles(&info, (const char**)filePaths, (u32)fileCount) == (s32)udtErrorCode::None) ? 0 : __LINE__;
+	const s32 errorCode = udtMergeDemoFiles(&info, (const char**)filePaths, (u32)fileCount);
+	if(errorCode != (s32)udtErrorCode::None)
+	{
+		fprintf(stderr, "udtMergeDemoFiles failed with error: '%s'\n", udtGetErrorCodeString(errorCode));
+		return false;
+	}
+
+	return true;
 }
 
 int udt_main(int argc, char** argv)
@@ -31,8 +46,13 @@ int udt_main(int argc, char** argv)
 	if(argc < 2)
 	{
 		PrintHelp();
+		return 0;
+	}
+
+	if(!MergeDemos(argv + 1, argc - 1))
+	{
 		return 1;
 	}
 
-	return MergeDemos(argv + 1, argc - 1);
+	return 0;
 }
