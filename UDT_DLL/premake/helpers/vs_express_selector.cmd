6ff21dc3505f274d8d83babc1a954dc102bec5cc
myT <mighty.cow@mailinator.com>
2015-12-03 01:30:29 +0100

    fix: typo

--- UDT_DLL/premake/helpers/vs_express_selector.cmd
+++ UDT_DLL/premake/helpers/vs_express_selector.cmd
@@ -1,4 +1,4 @@
-echo Visual Studio express? y for yes, anything else fo for no
+echo Visual Studio express? y for yes, anything else for no
 set /p choice=
 if %choice%==y (
 	set vs_exe=VCExpress.exe
