d5e701a1e6554a5fcea064d3891b4acebb59dbab
myT <mighty.cow@mailinator.com>
2015-12-03 04:15:30 +0100

    add: a new batch file to automate the build of profile-guided optimized binaries with Visual Studio

--- UDT_DLL/premake/helpers/vs_version_selector.cmd
+++ UDT_DLL/premake/helpers/vs_version_selector.cmd
@@ -8,18 +8,23 @@ set /p choice=
 if %choice%==1 (
 	set vs_generator=vs2013
 	set "vs_path=%VS120COMNTOOLS%"
+	set vs_version=12.0
 ) else if %choice%==2 (
 	set vs_generator=vs2012
 	set "vs_path=%VS110COMNTOOLS%"
+	set vs_version=11.0
 ) else if %choice%==3 (
 	set vs_generator=vs2010
 	set "vs_path=%VS100COMNTOOLS%"
+	set vs_version=10.0
 ) else if %choice%==4 (
 	set vs_generator=vs2008
 	set "vs_path=%VS90COMNTOOLS%"
+	set vs_version=9.0
 ) else if %choice%==5 (
 	set vs_generator=vs2005
 	set "vs_path=%VS80COMNTOOLS%"
+	set vs_version=8.0
 ) else (
     echo Invalid choice
 	exit
